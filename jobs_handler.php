bcdc9332872415465cb8b08945fb39c67a333570
doojit <doojit@me.com>
2023-10-28 07:38:53 +0100

    jobs

--- jobs_handler.php
+++ jobs_handler.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "auburnzq_admin";
-$password = "Platinum10**"; 
+$password = "Platinum10**";
 $database = "auburnzq_newsite";
 
 $conn = new mysqli($servername, $username, $password, $database);
@@ -10,7 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$action = $_GET['action'];
+$action = isset($_GET['action']) ? $_GET['action'] : (isset($_POST['action']) ? $_POST['action'] : '');
 
 switch ($action) {
     case 'getJobs':
@@ -19,19 +19,67 @@ switch ($action) {
         $jobs = [];
 
         if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
+            while ($row = $result->fetch_assoc()) {
                 $jobs[] = $row;
             }
         }
-        echo json_encode($jobs);
+        echo json_encode(['success' => true, 'data' => $jobs]);
         break;
 
-    // Add more cases here for other CRUD operations
-    // e.g., 'addJob', 'updateJob', 'deleteJob'
-    
+    case 'add':
+        if (isset($_POST['title']) && isset($_POST['description'])) {
+            $stmt = $conn->prepare("INSERT INTO jobs (job_title, job_description, status) VALUES (?, ?, 'active')");
+            $stmt->bind_param("ss", $_POST['title'], $_POST['description']);
+            if ($stmt->execute()) {
+                echo json_encode(['success' => true, 'message' => 'Job added successfully.']);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Error adding job.']);
+            }
+            $stmt->close();
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Invalid input.']);
+        }
+        break;
+
+    case 'edit':
+        // Assuming you pass job_title and job_description as POST parameters for editing along with ID.
+        if (isset($_POST['id']) && isset($_POST['title']) && isset($_POST['description'])) {
+            $stmt = $conn->prepare("UPDATE jobs SET job_title = ?, job_description = ? WHERE id = ?");
+            $stmt->bind_param("ssi", $_POST['title'], $_POST['description'], $_POST['id']);
+            if ($stmt->execute()) {
+                echo json_encode(['success' => true, 'message' => 'Job edited successfully.']);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Error editing job.']);
+            }
+            $stmt->close();
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Invalid input.']);
+        }
+        break;
+
+    case 'delete':
+        if (isset($_POST['id'])) {
+            $stmt = $conn->prepare("DELETE FROM jobs WHERE id = ?");
+            $stmt->bind_param("i", $_POST['id']);
+            if ($stmt->execute()) {
+                echo json_encode(['success' => true, 'message' => 'Job deleted successfully.']);
+            } else {
+                echo json_encode(['success' => false, 'message' => 'Error deleting job.']);
+            }
+            $stmt->close();
+        } else {
+            echo json_encode(['success' => false, 'message' => 'Invalid input.']);
+        }
+        break;
+
     default:
-        echo "Invalid action";
+        echo json_encode(['success' => false, 'message' => 'Invalid action.']);
 }
+
+$conn->close();
+?>
+
+
 
 $conn->close();
 ?>
